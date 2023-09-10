24e4b4e5ad575237cd620d946502882dfacb7e54
Alex-chulu <chulualexander@gmail.com>
2023-09-10 20:08:26 +0000

    Donot make a promise...if you know you cannot keep it

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (success) {
+        const response = {
+          status: 200,
+          body: 'Success',
+        };
+        resolve(response);
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
+    }, 1000); // Simulate a 1-second delay
+  });
+}
