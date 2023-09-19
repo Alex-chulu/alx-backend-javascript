1a0a11920f89826cb0b1f165f27d4d776fb9556b
Alex-chulu <chulualexander@gmail.com>
2023-09-19 11:33:54 +0000

    list student id

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,18 @@
+function getListStudentIds(arrayOfStudents) {
+  // Check if the input is an array
+  if (!Array.isArray(arrayOfStudents)) {
+    return [];
+  }
+
+  // Use the map function to extract student IDs
+  const studentIds = arrayOfStudents.map(student => student.id);
+
+  return studentIds;
+}
+
+// Example usage:
+// const students = getListStudents(); // Get the list of students
+// const studentIds = getListStudentIds(students); // Get the list of student IDs
+
+module.exports = getListStudentIds;
+
