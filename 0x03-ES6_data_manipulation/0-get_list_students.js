db0092f8df3c09de000a2504530b83bdd53de035
Alex-chulu <chulualexander@gmail.com>
2023-09-19 11:30:41 +0000

    list students

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,12 @@
+// Define a function named getListStudents
 function getListStudents() {
-  const students = [
+  // Return an array of objects
+  return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-
-  return students;
 }
+
+// Export the function to make it accessible to other modules
+module.exports = getListStudents;
