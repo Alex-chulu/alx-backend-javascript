8ae5087ecaecb46ab461804582cc4f7b0edc6e62
Alex-chulu <chulualexander@gmail.com>
2023-09-19 20:56:43 +0000

    skip test

---
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -0,0 +1,14 @@
+describe('Your Test Suite', () => {
+  it('should do something', () => {
+    // Your test code here
+  });
+
+  it.skip('should be skipped', () => {
+    // This test will be skipped
+  });
+
+  it('should do something else', () => {
+    // Your test code here
+  });
+});
+
