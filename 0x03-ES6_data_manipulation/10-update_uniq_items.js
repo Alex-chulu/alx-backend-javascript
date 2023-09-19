2e062f4befaf7af5f1dd0094f9f5277952d4381a
Alex-chulu <chulualexander@gmail.com>
2023-09-19 19:58:49 +0000

    updateuniqu

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,16 @@
+function updateUniqueItems(inputMap) {
+  if (!(inputMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [item, quantity] of inputMap.entries()) {
+    if (quantity === 1) {
+      inputMap.set(item, 100);
+    }
+  }
+
+  return inputMap;
+}
+
+module.exports = updateUniqueItems;
+
