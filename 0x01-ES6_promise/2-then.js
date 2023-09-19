404c65f5cb068bb1df0b9120a50f5a87d07b8e78
Alex-chulu <chulualexander@gmail.com>
2023-09-19 11:05:13 +0000

    cat me fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,11 @@
 function handleResponseFromAPI(promise) {
   return promise
-    .then((result) => {
+    .then(() => {
       console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success',
-      };
+      return { status: 200, body: 'success' };
     })
-    .catch((error) => {
-      console.error('Got an error from the API');
+    .catch(() => {
+      console.log('Got a response from the API');
       return new Error();
     });
 }
-
