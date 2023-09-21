b791ed8be399d7dd0b37f07a076c56d8bc0a86d7
Alex-chulu <chulualexander@gmail.com>
2023-09-21 22:04:31 +0000

    http

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,15 @@
 const http = require('http');
 
+// Create an HTTP server
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!\n');
 });
 
+// Listen on port 1245
 app.listen(1245);
 
+// Export the app
 module.exports = app;
 
