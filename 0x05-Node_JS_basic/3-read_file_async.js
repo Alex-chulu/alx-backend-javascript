0dd1b22a8e22f18c58339689166dc042ab3c8616
Alex-chulu <chulualexander@gmail.com>
2023-09-21 22:49:14 +0000

    readfiel

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,37 +1,27 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf-8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      const lines = data.split('\n').filter(line => line.trim() !== '');
-
-      const fields = {};
-      lines.forEach(line => {
-        const student = line.split(',');
-        const field = student[3];
-        if (field) {
-          if (!fields[field]) fields[field] = [];
-          fields[field].push(student[0]);
+  const promise = (res, rej) => {
+    fs.readFile(path, (err, data) => {
+      if (err) rej(Error('Cannot load the database'));
+      if (data) {
+        let newD = data.toString().split('\n');
+        newD = newD.slice(1, newD.length - 1);
+        console.log(`Number of students: ${newD.length}`);
+        const obj = {};
+        newD.forEach((el) => {
+          const student = el.split(',');
+          if (!obj[student[3]]) obj[student[3]] = [];
+          obj[student[3]].push(student[0]);
+        });
+        for (const cls in obj) {
+          if (cls) console.log(`Number of students in ${cls}: ${obj[cls].length}. List: ${obj[cls].join(', ')}`);
         }
-      });
-
-      const totalStudents = lines.length - 1; // Subtract 1 to exclude the header line
-      const result = [`Number of students: ${totalStudents}`];
-
-      Object.keys(fields).forEach(field => {
-        result.push(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
-      });
-
-      console.log(result.join('\n'));
-      resolve(result.join('\n'));
+      }
+      res();
     });
-  });
+  };
+  return new Promise(promise);
 }
 
 module.exports = countStudents;
-
