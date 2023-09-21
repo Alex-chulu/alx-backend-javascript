2f4bebaec7d238bb492dbc40b06d9a3a9f51d7d7
Alex-chulu <chulualexander@gmail.com>
2023-09-21 22:16:21 +0000

    http fix

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,59 +1,36 @@
 const http = require('http');
-const { readDB } = require('./3-read_file_async');
 
-// Create an HTTP server
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = args[0];
+
+const hostname = 'localhost';
+const port = 1245;
+
 const app = http.createServer(async (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
-  if (req.url === '/') {
-    res.end('Hello Holberton School!\n');
-  } else if (req.url === '/students') {
+  const { url } = req;
+
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
     try {
-      const data = await readDB(process.argv[2]);
-      const lines = data.toString().split('\n').filter(Boolean);
-      const fields = lines.shift().split(',');
-
-      if (fields.length !== 4 || !fields.includes('firstname') || !fields.includes('lastname') || !fields.includes('age') || !fields.includes('field')) {
-        res.end('Invalid data format in the database\n');
-        return;
-      }
-
-      const students = {};
-      const numStudents = {};
-
-      lines.forEach((line) => {
-        const [firstname, lastname, age, field] = line.split(',').map((item) => item.trim());
-
-        if (field in students) {
-          students[field].push(firstname);
-        } else {
-          students[field] = [firstname];
-        }
-
-        if (field in numStudents) {
-          numStudents[field] += 1;
-        } else {
-          numStudents[field] = 1;
-        }
-      });
-
-      res.write('This is the list of our students\n');
-      res.write(`Number of students: ${lines.length}\n`);
-
-      Object.entries(numStudents).forEach(([field, count]) => {
-        res.write(`Number of students in ${field}: ${count}. List: ${students[field].join(', ')}\n`);
-      });
-
-      res.end();
-    } catch (err) {
-      res.end('Cannot load the database\n');
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
     }
-  } else {
-    res.end('Not Found\n');
   }
+  res.statusCode = 404;
+  res.end();
 });
 
-app.listen(1245, 'localhost');
+app.listen(port, hostname, () => {
+
+});
 
 module.exports = app;
-
