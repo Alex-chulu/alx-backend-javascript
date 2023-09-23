46417343641d002a76b33bf0c5cade6bd27040ef
Alex-chulu <chulualexander@gmail.com>
2023-09-23 13:34:09 +0000

    pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,37 @@
+// 4-pricing.js
+
+const Currency = require('./3-currency'); // Import the Currency class from 3-currency.js
+
+class Pricing {
+    constructor(amount, currency) {
+        this._amount = amount;
+        this._currency = currency;
+    }
+
+    get amount() {
+        return this._amount;
+    }
+
+    set amount(newAmount) {
+        this._amount = newAmount;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+
+    set currency(newCurrency) {
+        this._currency = newCurrency;
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    }
+
+    static convertPrice(amount, conversionRate) {
+        return amount * conversionRate;
+    }
+}
+
+module.exports = Pricing;
+
