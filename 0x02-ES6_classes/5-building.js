605ac4e85dafe09f84ea8cf2e0f6755fc81d72e2
Alex-chulu <chulualexander@gmail.com>
2023-09-23 13:58:49 +0000

    building fix

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,9 @@
 export default class Building {
-  constructor(sqft) {
+  constructor(sqft = 0) {
+    if (this.constructor !== Building &&
+	typeof this.evacuationWarningMessage !== 'function'){
+	throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
