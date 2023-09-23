4ab29ede7f54a5dece40c0585564568860cf79f5
Alex-chulu <chulualexander@gmail.com>
2023-09-23 14:08:57 +0000

    sky high

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,17 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft); // Call the parent class constructor with the sqft parameter
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
+
