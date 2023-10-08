c6ad023fb75a6588aca069b3a59c9f6302794f1d
Alex-chulu <chulualexander@gmail.com>
2023-10-08 14:10:53 +0000

    skyhigh

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,17 +1,17 @@
 import Building from './5-building';
-/* eslint-disable no-underscore-dangle */
+
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this.floors = floors;
   }
-/* set and get floors*/
+
   get floors() {
     return this._floors;
   }
 
-  set floors(value) {
-    this._floors = value;
+  set floors(input) {
+    this._floors = input;
   }
 
   evacuationWarningMessage() {
