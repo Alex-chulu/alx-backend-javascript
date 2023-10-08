72d8e4f43559e5981a742b8b2ed5317f3ad21998
Alex-chulu <chulualexander@gmail.com>
2023-10-08 09:29:54 +0000

    task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,4 @@
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // You can perform asynchronous operations here
-    // For example, making an API request
-    // Once the operation is complete, call resolve with the result
-    // If an error occurs, call reject with the error message
-  });
+  return new Promise((resolve, reject) => {});
 }
 
