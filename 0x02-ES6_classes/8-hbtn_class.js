d11a4063d77fdf9622a513ec06237ea0a1f66553
Alex-chulu <chulualexander@gmail.com>
2023-10-08 14:24:14 +0000

    hbtn

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,15 +1,33 @@
+/* HolbertonClass */
 export default class HolbertonClass {
   constructor(size, location) {
     this._size = size;
     this._location = location;
   }
 
-  valueOf() {
+  set size(input) {
+    this._size = input;
+  }
+
+  get size() {
     return this._size;
   }
 
-  toString() {
+  set location(input) {
+    this._location = input;
+  }
+
+  get location() {
     return this._location;
   }
-}
 
+  [Symbol.toPrimitive](dailer) {
+    if (dailer === 'string') {
+      return this.location;
+    }
+    if (dailer === 'number') {
+      return this.size;
+    }
+    return null;
+  }
+}
