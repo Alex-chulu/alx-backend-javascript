db7e713c50fb7a70952575dc29b22c3db26996f7
Alex-chulu <chulualexander@gmail.com>
2023-10-08 11:06:29 +0000

    task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -2,10 +2,9 @@ export default function guardrail(mathFunction) {
   const queue = [];
 
   try {
-    const result = mathFunction();
-    queue.push(result);
+    queue.push(mathFunction());
   } catch (err) {
-    queue.push(String(err));
+    queue.push(`Error: ${ err.message}`);
   } finally {
     queue.push('Guardrail was processed');
   }
