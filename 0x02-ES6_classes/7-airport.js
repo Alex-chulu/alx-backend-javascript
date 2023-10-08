280bea4367cadf68c9e066c8ac39a84bbab200c0
Alex-chulu <chulualexander@gmail.com>
2023-10-08 13:09:33 +0000

    airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -3,23 +3,9 @@ export default class Airport {
     this._name = name;
     this._code = code;
   }
-  get name() {
-    return this._name;
-  }
-
-  set name(value) {
-    this._name = value;
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(value) {
-    this._code = value;
-  }
 
   toString() {
-    return `[${this._code}]`;
+    return `[${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
   }
 }
+
